6d48548054dfe42fc47de457a0a2613bc465babf
feuilleCOeur <145110507+feuilleCOeur@users.noreply.github.com>
2024-03-08 15:34:54 +0900

    ポートフォリオ
    ポートフォリオ共有

---
+++ script.js
@@ -0,0 +1,135 @@
+// タブ切り替え
+$(function(){
+    $('.tab-menu').on('click',function(){
+      let index = $('.tab-menu').index(this);
+      console.log(index);
+      
+      $(this).addClass('is-active').siblings('.tab-menu').removeClass('is-active');
+      
+      $(this).closest('.tab-menus').next('.tab-contents').find('.tab-content').removeClass('is-active');
+      
+      $('.tab-content').eq(index).addClass('is-active');
+    });
+  });
+  
+
+// スクロールエフェクト
+  $(window).scroll(function (){
+    //動きの指定
+    let scroll = $(window).scrollTop();
+    // console.log(scroll);
+    let windowHeight = $(window).height();
+    $('.fadeUpTrigger').each(function(){ //fadeInTriggerというクラス名が
+        let elemPos = $(this).offset().top + 200;//要素より、200px上の
+        if (scroll >= elemPos - windowHeight){
+          $(this).addClass('fadeUp');// 画面内に入ったらfadeInというクラス名を追記
+        }else{
+          $(this).removeClass('fadeUp');// 画面外に出たらfadeInというクラス名を外す
+        }
+    });
+});
+
+
+
+// eachTextAnimeにappeartextというクラス名を付ける定義
+function EachTextAnimeControl() {
+  $('.eachTextAnime').each(function () {
+    var elemPos = $(this).offset().top - 50;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass("appeartext");
+
+    } else {
+      $(this).removeClass("appeartext");
+    }
+  });
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+  EachTextAnimeControl();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+
+
+
+
+
+
+// TextTypingというクラス名がついている子要素（span）を表示から非表示にする定義
+function TextTypingAnime() {
+	$('.TextTyping').each(function () {
+		var elemPos = $(this).offset().top - 50;
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		var thisChild = "";
+		if (scroll >= elemPos - windowHeight) {
+			thisChild = $(this).children(); //spanタグを取得
+			//spanタグの要素の１つ１つ処理を追加
+			thisChild.each(function (i) {
+				var time = 100;
+				//時差で表示する為にdelayを指定しその時間後にfadeInで表示させる
+				$(this).delay(time * i).fadeIn(time);
+			});
+		} else {
+			thisChild = $(this).children();
+			thisChild.each(function () {
+				$(this).stop(); //delay処理を止める
+				$(this).css("display", "none"); //spanタグ非表示
+			});
+		}
+	});
+}
+
+
+
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+	//spanタグを追加する
+	var element = $(".TextTyping");
+	element.each(function () {
+		var text = $(this).html();
+		var textbox = "";
+		text.split('').forEach(function (t) {
+			if (t !== " ") {
+				textbox += '<span>' + t + '</span>';
+			} else {
+				textbox += t;
+			}
+		});
+		$(this).html(textbox);
+
+	});
+
+	TextTypingAnime();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
+
+$('.slider').slick({
+  fade:true,//切り替えをフェードで行う。初期値はfalse。
+  autoplay: true,//自動的に動き出すか。初期値はfalse。
+  autoplaySpeed: 5000,//次のスライドに切り替わる待ち時間
+  speed:2000,//スライドの動きのスピード。初期値は300。
+  infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+  slidesToShow: 1,//スライドを画面に3枚見せる
+  slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
+  arrows: true,//左右の矢印あり
+  prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+  nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+});
+
+//スマホ用：スライダーをタッチしても止めずにスライドをさせたい場合
+$('.slider').on('touchmove', function(event, slick, currentSlide, nextSlide){
+  $('.slider').slick('slickPlay');
+});
+
+
+
+// ハンバーガーメニュー
+
+$(".openbtn").click(function () {
+	$(this).toggleClass('active');
+	$(".nav").toggleClass('active');
+  });
